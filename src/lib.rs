d284391ad75ec6af0fa62629e44975dd9bcc847e
Dmitry Shapkin <dvshapkin@mail.ru>
2020-05-23 13:45:54 +0700

    improved screen output

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 /// Measures the average code execution time in N repetitions.
+/// 
 /// `bench!(wrapper_func, N, "Prompt")` execute `wrapper_func` N repetitions.
+/// 
 /// `bench!(wrapper_func, "Prompt")` execute `wrapper_func` 10,000 repetitions.
+/// 
 /// The result prints to the standard output as `Prompt: xxx.yy ms`.
 #[macro_export]
 macro_rules! bench {
@@ -13,8 +16,13 @@ macro_rules! bench {
         for _ in 0..n {
             $func();
         }
-        let stop = std::time::Instant::now();
-        println!("{}: {:?}", $description, stop.saturating_duration_since(start) / n)
+        let elapsed = start.elapsed() / n;
+        if elapsed.as_secs() > 60 {
+            println!("{}: {} s", $description, elapsed.as_secs_f32())
+        } else {
+            println!("{}: {:?}", $description, elapsed)
+        }
+        
     }
 }
 
